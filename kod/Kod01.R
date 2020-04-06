523b00ca272d826741da967cccfe7310fb9510fc
mzeybek <muzo583@gmail.com>
2020-04-06 21:53:10 +0300

    Create Kod01.R

---
+++ kod/Kod01.R
@@ -0,0 +1,13 @@
+#RHaritaKitap Kod01.R
+
+getwd() # RStudio programı ve R çalışma klasörünü gösterir.
+
+setwd("Desktop/") # Çalışma klasörünü masaüstü olarak ayarlama
+
+poligon <- read.csv(file="data/data01.csv", header=TRUE, sep="\t"); 
+# read.csv fonksiyonu
+# kullanarak masaüstünde yer alan data klasörü içinden data01.csv isimli dosyanın başlığıyla 
+# birlikte tab aralıklı olarak poligon nesnesine atanır. 
+poligon
+#poligon nesnesinin özelliği
+class(poligon)
