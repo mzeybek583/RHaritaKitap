e4527c5c3c8afc1dd095808cac0d0dc8208b73b0
mzeybek <muzo583@gmail.com>
2020-04-06 22:02:59 +0300

    Update Kod01.R

--- kod/Kod01.R
+++ kod/Kod01.R
@@ -2,11 +2,14 @@
 
 getwd() # RStudio programı ve R çalışma klasörünü gösterir.
 
-setwd("Desktop/") # Çalışma klasörünü masaüstü olarak ayarlama
+setwd("Desktop/") # Çalışma klasörünü masaüstü olarak 
+#ayarlama
 
-poligon <- read.csv(file="data/data01.csv", header=TRUE, sep="\t"); 
+poligon <- read.csv(file="data/data01.csv", header=TRUE, 
+                    sep="\t"); 
 # read.csv fonksiyonu
-# kullanarak masaüstünde yer alan data klasörü içinden data01.csv isimli dosyanın başlığıyla 
+# kullanarak masaüstünde yer alan data klasörü içinden 
+#data01.csv isimli dosyanın başlığıyla 
 # birlikte tab aralıklı olarak poligon nesnesine atanır. 
 poligon
 #poligon nesnesinin özelliği
