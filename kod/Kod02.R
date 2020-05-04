93c6716608f9fda1ba336f4104d1979cc0e00799
mzeybek583 <muzo583@gmail.com>
2020-05-04 04:05:54 +0300

    Kod03.R ekleme

--- kod/Kod02.R
+++ kod/Kod02.R
@@ -1,5 +1,8 @@
+#RHaritaKitap Kod02.R
 
 # Temel İstatistik
 x <- c(44.4, 45.9, 41.9, 53.3, 44.7, 44.1, 50.7, 45.2, 60.1)
+
 y <- c( 2.6, 3.1, 2.5, 5.0, 3.6, 4.0, 5.2, 2.8, 3.8)
+
 cor.test(x,y,method="pearson",alternative="two.sided",conf.level = 0.95)
---
+++ kod/Kod03.R
@@ -0,0 +1,35 @@
+#RHaritaKitap Kod03.R
+
+# install.packages("tidyverse", "ggpubr") # ek paketlerin kurulumu
+
+library(tidyverse)
+library(ggpubr)
+
+set.seed(4208)
+
+theme_set(theme_pubr())
+
+# 1'den 100'e kadar 1'er artışla veri oluşturma
+def <- seq(1:100) 
+
+n <- rnorm(100, mean = 0.5, sd = 1) # Gürültü oluşturma
+
+def <- def + n # Gürültü ekleme
+
+term <- rnorm(100, mean=25, sd= 5)
+
+term <- sort(term)
+
+df <- data.frame(def,term)
+
+# Veri kontrol
+head(df)
+
+#Çizdirme
+ggplot(df, aes(x = def, y = term)) +
+  geom_point() +
+  stat_smooth() + xlab("Deformasyon [mm]") +
+  ylab("Sıcaklık [°C]")
+
+# Korelasyon katsayısı
+cor(df$def,df$term)
